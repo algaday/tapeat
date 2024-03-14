825bc0a2d2282545d4dcf587a117970c661fef9d
Algaday <algadai.kanatbek@gmail.com>
2024-03-14 23:15:35 +0500

    Error handler improvment

--- src/common/filters/http-exception/http-exception.filter.ts
+++ src/common/filters/http-exception/http-exception.filter.ts
@@ -4,23 +4,36 @@ import {
   ExceptionFilter,
   HttpException,
 } from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 import { Request, Response } from 'express';
 import { ApplicationError } from 'src/errors';
 
-@Catch(HttpException)
-export class HttpExceptionFilter<T extends HttpException>
-  implements ExceptionFilter
-{
+@Catch()
+export class HttpExceptionFilter<T> implements ExceptionFilter {
   catch(exception: T, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
-    const request = ctx.getRequest<Request>();
-    const status = exception.getStatus();
-    console.log(response);
-    response.status(status).json({
-      statusCode: status,
+    if (exception instanceof HttpException) {
+      const status = exception.getStatus();
+
+      return response.status(status).json({
+        statusCode: status,
+        timestamp: new Date().toISOString(),
+      });
+    }
+    if (exception instanceof ApplicationError) {
+      const status = 400;
+
+      return response.status(status).json({
+        statusCode: status,
+        message: String(exception),
+        timestamp: new Date().toISOString(),
+      });
+    }
+    return response.status(500).json({
+      statusCode: 500,
+      exception,
       timestamp: new Date().toISOString(),
-      path: request.url,
     });
   }
 }
--- src/user/errors/user-already-exists.error.ts
+++ src/user/errors/user-already-exists.error.ts
@@ -1,10 +1,8 @@
 import { ApplicationError } from 'src/errors';
 
 export class UserAlreadyExistsError extends ApplicationError {
-  private statusCode: number;
   constructor(message) {
     super(message);
     this.name = 'UserExistsError';
-    this.statusCode = 403;
   }
 }
