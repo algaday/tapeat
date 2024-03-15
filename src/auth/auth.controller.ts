42da0892326dc9dae824c5f850cf7336e73fb845
Algaday <algadai.kanatbek@gmail.com>
2024-03-15 11:50:44 +0500

    Added route JWT guards

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,16 +2,19 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 import { UserDto } from 'src/user/dto';
+import { Public } from 'src/common/decorators';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @Public()
   @Post('signup')
   signup(@Body() dto: UserDto) {
     return this.authService.signup(dto);
   }
 
+  @Public()
   @Post('signin')
   signin(@Body() dto: AuthDto) {
     return this.authService.signin(dto);
