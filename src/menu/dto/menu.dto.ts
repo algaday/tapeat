48c13aed0da7e20f73e08c5180edadde49e920f3
Algaday <algadai.kanatbek@gmail.com>
2024-03-21 09:06:25 +0500

    added Variation class in MenuDto

--- src/menu/dto/menu.dto.ts
+++ src/menu/dto/menu.dto.ts
@@ -1,4 +1,33 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
+import {
+  IsArray,
+  IsDefined,
+  IsNotEmpty,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+
+class Variation {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsDefined()
+  @IsArray()
+  @ValidateNested()
+  @Type(() => VariationOptions)
+  options: VariationOptions[];
+}
+
+class VariationOptions {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  price: number;
+}
 
 export class MenuDto {
   @IsString()
@@ -20,4 +49,9 @@ export class MenuDto {
   @IsString()
   @IsNotEmpty()
   price: string;
+
+  @IsArray()
+  @ValidateNested()
+  @Type(() => Variation)
+  variation?: Variation[];
 }
