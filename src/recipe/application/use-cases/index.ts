ef5cfaab290046cbc734b854be6ce9460c2de4cf
Ansar <annssarr@gmail.com>
2024-10-10 17:03:50 +0500

    feat: recipe ingredients service

---
+++ src/recipe/application/use-cases/index.ts
@@ -0,0 +1,15 @@
+import { CreateRecipeUseCase } from './create-recipe/create-recipe.use-case';
+import { UpdateRecipeUseCase } from './update-recipe/update-recipe.use-case';
+import { GetRecipeUseCase } from './get-recipe/get-recipe.use-case';
+import { DeleteRecipeUseCase } from './delete-recipe/delete-recipe.use-case';
+import { AddRecipeIngredientsUseCase } from './add-recipe-ingredients/add-recipe-ingredient.use-case';
+import { RecipeIngredientService } from '../services/recipe-ingredient.service';
+
+export const RECIPE_USE_CASES = [
+  CreateRecipeUseCase,
+  UpdateRecipeUseCase,
+  GetRecipeUseCase,
+  DeleteRecipeUseCase,
+  AddRecipeIngredientsUseCase,
+];
+export const RECIPE_APPLICATION_SERVICES = [RecipeIngredientService];
