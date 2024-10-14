78f127e00a6dc2e87a3119484137a1354b3dc8d8
Ansar <annssarr@gmail.com>
2024-10-14 21:21:07 +0500

    fix: error message text style

--- src/recipe/errors/recipe-ingredient-not-found.error.ts
+++ src/recipe/errors/recipe-ingredient-not-found.error.ts
@@ -1,5 +1,5 @@
 import { DomainError } from 'src/core/domain/domain-error.base';
 
 export class RecipeIngredientNotFoundError extends DomainError {
-  code: string = 'recipe_ingredient_not_found';
+  code: string = 'RECIPE_INGREDIENT.NOT_FOUND';
 }
--- src/recipe/errors/recipe-not-found.error.ts
+++ src/recipe/errors/recipe-not-found.error.ts
@@ -1,5 +1,5 @@
 import { DomainError } from 'src/core/domain/domain-error.base';
 
 export class RecipeNotFoundError extends DomainError {
-  code: string = 'recipe_not_found';
+  code: string = 'RECIPE.NOT_FOUND';
 }
