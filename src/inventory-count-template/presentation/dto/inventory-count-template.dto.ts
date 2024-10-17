74758be416aade3527238108ae765551dba494e4
Ansar <annssarr@gmail.com>
2024-10-17 23:15:21 +0500

    feat: dtos for inventory count template

---
+++ src/inventory-count-template/presentation/dto/inventory-count-template.dto.ts
@@ -0,0 +1,11 @@
+import { IsArray, IsEnum, IsString } from 'class-validator';
+import { InventoryCountTemplateType } from 'src/inventory-count-template/domain/inventory-count-template.entity';
+
+export class InventoryCountTemplateDto {
+  @IsString()
+  id: string;
+  @IsEnum(InventoryCountTemplateType)
+  templateType: InventoryCountTemplateType;
+  @IsArray()
+  storageIds: string[];
+}
