3bdb59edf8751828ad073acf817f305389dfeef0
Ansar <annssarr@gmail.com>
2024-10-20 18:19:22 +0500

    feat: create use case for inventory count

---
+++ src/inventory-count/application/use-cases/create-inventory-count/create-inventory-count.use-case.ts
@@ -0,0 +1,32 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { UseCase } from 'src/core/domain/use-case.interface';
+import { InventoryCountRepositoryPort } from 'src/inventory-count/domain/inventory-count-repository.port';
+import { InventoryCountEntity } from 'src/inventory-count/domain/inventory-count.entity';
+import { InventoryCountDto as InventoryCountUi } from 'src/inventory-count/presentation/dto/inventory-count.dto';
+import { InventoryCountMapper } from '../../mappers/inventory-count.mapper';
+
+interface Props {
+  inventoryCountTemplateId: string;
+  staffName: string;
+}
+
+@Injectable()
+export class CreateInventoryCountUseCase
+  implements UseCase<Props, InventoryCountUi>
+{
+  constructor(
+    private readonly mapper: InventoryCountMapper,
+
+    @Inject(InventoryCountRepositoryPort)
+    private readonly inventoryCountRepository: InventoryCountRepositoryPort,
+  ) {}
+
+  async execute(props: Props): Promise<InventoryCountUi> {
+    const inventoryCount = InventoryCountEntity.create({
+      ...props,
+      inventoryCountItems: [],
+    });
+    await this.inventoryCountRepository.create(inventoryCount);
+    return this.mapper.toUi(inventoryCount);
+  }
+}
---
+++ src/inventory-count/application/use-cases/index.ts
@@ -0,0 +1,3 @@
+import { CreateInventoryCountUseCase } from './create-inventory-count/create-inventory-count.use-case';
+
+export const INVENTORY_COUNT_USE_CASES = [CreateInventoryCountUseCase];
