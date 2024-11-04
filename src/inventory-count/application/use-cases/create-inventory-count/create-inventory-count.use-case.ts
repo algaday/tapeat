5f7f815e5b2f3e8e6f47670e2b7628111a3c8326
Ansar <annssarr@gmail.com>
2024-11-04 17:06:54 +0500

    refactor: create inventory count use case

--- src/inventory-count/application/use-cases/create-inventory-count/create-inventory-count.use-case.ts
+++ src/inventory-count/application/use-cases/create-inventory-count/create-inventory-count.use-case.ts
@@ -1,8 +1,15 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { UseCase } from 'src/core/domain/use-case.interface';
+import { InventoryCountTemplateRepositoryPort } from 'src/inventory-count-template/domain/inventory-count-template-repository.port';
+import { InventoryCountTemplateNotFoundError } from 'src/inventory-count-template/errors/inventory-count-template-not-found.error';
+import { InventoryCountItemEntity } from 'src/inventory-count/domain/inventory-count-item.entity';
 import { InventoryCountRepositoryPort } from 'src/inventory-count/domain/inventory-count-repository.port';
-import { InventoryCountEntity } from 'src/inventory-count/domain/inventory-count.entity';
-import { InventoryCountDto as InventoryCountUi } from 'src/inventory-count/presentation/dto/inventory-count.dto';
+import {
+  InventoryCountEntity,
+  InventoryCountItemType,
+} from 'src/inventory-count/domain/inventory-count.entity';
+import { InventoryCountWithStorageDto as InventoryCountUi } from 'src/inventory-count/presentation/dto/inventory-count.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { InventoryCountMapper } from '../../mappers/inventory-count.mapper';
 
 interface Props {
@@ -16,19 +23,55 @@ export class CreateInventoryCountUseCase
 {
   constructor(
     private readonly mapper: InventoryCountMapper,
+    private readonly prisma: PrismaService,
 
     @Inject(InventoryCountRepositoryPort)
     private readonly inventoryCountRepository: InventoryCountRepositoryPort,
+
+    @Inject(InventoryCountTemplateRepositoryPort)
+    private readonly inventoryCountTemplateRepository: InventoryCountTemplateRepositoryPort,
   ) {}
 
   async execute(props: Props): Promise<InventoryCountUi> {
-    const inventoryCount = InventoryCountEntity.create({
-      ...props,
-      inventoryCountItems: [],
+    return await this.prisma.$transaction(async () => {
+      const detailedTemplate =
+        await this.inventoryCountTemplateRepository.findTemplateStoragesWithItems(
+          props.inventoryCountTemplateId,
+        );
+
+      if (!detailedTemplate) {
+        throw new InventoryCountTemplateNotFoundError(
+          `InventoryCountTemplate with ID ${props.inventoryCountTemplateId} not found.`,
+        );
+      }
+
+      const inventoryCountItems = detailedTemplate.storages.flatMap(
+        (templateStorage) =>
+          templateStorage.storage.items.map((item) =>
+            InventoryCountItemEntity.create({
+              storageName: templateStorage.storage.name,
+              quantity: null,
+              itemId: item.ingredientId ?? item.recipeId,
+              type: item.ingredientId
+                ? InventoryCountItemType.INGREDIENT
+                : InventoryCountItemType.RECIPE,
+            }),
+          ),
+      );
+
+      const inventoryCount = InventoryCountEntity.create({
+        ...props,
+        inventoryCountItems,
+      });
+
+      await this.inventoryCountRepository.create(inventoryCount);
+
+      await this.inventoryCountRepository.createInventoryCountItems({
+        items: inventoryCountItems,
+        inventoryCountId: inventoryCount.getId(),
+      });
+
+      return this.mapper.toUi(inventoryCount);
     });
-
-    await this.inventoryCountRepository.create(inventoryCount);
-
-    return this.mapper.toUi(inventoryCount);
   }
 }
--- src/inventory-count/application/use-cases/index.ts
+++ src/inventory-count/application/use-cases/index.ts
@@ -1,12 +1,16 @@
 import { InventoryCountItemService } from '../services/inventory-count-item.service';
 import { CreateInventoryCountUseCase } from './create-inventory-count/create-inventory-count.use-case';
 import { GetInventoryCountUseCase } from './get-inventory-count/get-inventory-count.use-case';
+import { GetInventoryCountsUseCase } from './get-inventory-counts/get-inventory-counts.use-case';
+import { UpdateInventoryCountItemUseCase } from './update-inventory-count-item/update-inventory-count-item.use-case';
 import { UpdateInventoryCountUseCase } from './update-inventory-count/update-inventory-count.use-case';
 
 export const INVENTORY_COUNT_USE_CASES = [
   CreateInventoryCountUseCase,
   UpdateInventoryCountUseCase,
   GetInventoryCountUseCase,
+  UpdateInventoryCountItemUseCase,
+  GetInventoryCountsUseCase,
 ];
 
 export const INVENTORY_COUNT_APPLICATION_SERVICES = [InventoryCountItemService];
