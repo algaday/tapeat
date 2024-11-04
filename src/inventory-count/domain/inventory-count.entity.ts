339f49bb6dcb324538c07c53afe5f910e9d2dca4
Ansar <annssarr@gmail.com>
2024-11-04 17:06:12 +0500

    feat: inventory count status added to entity

--- src/inventory-count/domain/inventory-count.entity.ts
+++ src/inventory-count/domain/inventory-count.entity.ts
@@ -4,14 +4,29 @@ import { InventoryCountItemEntity } from './inventory-count-item.entity';
 interface Props {
   staffName: string;
   inventoryCountTemplateId: string;
+  status: InventoryCountStatus;
   inventoryCountItems: InventoryCountItemEntity[];
 }
+
 export enum InventoryCountItemType {
   INGREDIENT = 'ingredient',
   RECIPE = 'recipe',
 }
+
+export enum InventoryCountStatus {
+  PENDING = 'pending',
+  AWAITING_APPROVAL = 'awaitingApproval',
+  APPROVED = 'approved',
+  REJECTED = 'rejected',
+}
+
 export class InventoryCountEntity extends Entity<Props> {
-  static create(props: Props) {
-    return new InventoryCountEntity({ props });
+  static create(props: Omit<Props, 'status'> & Partial<Pick<Props, 'status'>>) {
+    const defaultedProps: Props = {
+      ...props,
+      status: props.status ?? InventoryCountStatus.PENDING,
+    };
+
+    return new InventoryCountEntity({ props: defaultedProps });
   }
 }
