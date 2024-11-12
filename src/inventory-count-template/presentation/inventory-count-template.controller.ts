746364101944070e446e2da84d2d8a874032210e
Ansar <annssarr@gmail.com>
2024-11-13 00:23:32 +0500

    feat: getTemplateByBranch method

--- src/inventory-count-template/presentation/inventory-count-template.controller.ts
+++ src/inventory-count-template/presentation/inventory-count-template.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { AssignStorageUseCase } from '../application/use-cases/assign-storage/assign-storage.use-case';
 import { CreateInventoryCountTemplateUseCase } from '../application/use-cases/create-inventory-count-template/create-inventory-count-template.use-case';
+import { GetBranchInventoryCountTemplatesUseCase } from '../application/use-cases/get-branch-inventory-count-templates/get-branch-inventory-count-templates.use-case';
 import { GetInventoryCountTemplateUseCase } from '../application/use-cases/get-inventory-count-template/get-inventory-count-template.use-case';
 import { CreateInventoryCountTemplateDto } from './dto/create-inventory-count-template.dto';
 
@@ -10,6 +11,7 @@ export class InventoryCountTemplateController {
     private readonly createInventoryCountTemplate: CreateInventoryCountTemplateUseCase,
     private readonly getInventoryCountTemplate: GetInventoryCountTemplateUseCase,
     private readonly assignStorage: AssignStorageUseCase,
+    private readonly getBranchInventoryCountTemplatesUseCase: GetBranchInventoryCountTemplatesUseCase,
   ) {}
   @Post()
   async create(@Body() dto: CreateInventoryCountTemplateDto) {
@@ -29,5 +31,10 @@ export class InventoryCountTemplateController {
     @Param('storageId') storageId: string,
   ) {
     return this.assignStorage.execute({ inventoryCountTemplateId, storageId });
+  }
+
+  @Get('branches/:branchId')
+  async getTemplatesByBranch(@Param('branchId') branchId: string) {
+    return this.getBranchInventoryCountTemplatesUseCase.execute({ branchId });
   }
 }
