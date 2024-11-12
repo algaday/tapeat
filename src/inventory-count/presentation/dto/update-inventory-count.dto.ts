38d17f96060cab55ad982e2db4768904a656bf24
Ansar <annssarr@gmail.com>
2024-11-13 00:21:40 +0500

    feat: dtos

--- src/inventory-count/presentation/dto/update-inventory-count.dto.ts
+++ src/inventory-count/presentation/dto/update-inventory-count.dto.ts
@@ -1,7 +1,14 @@
-import { IsNumber } from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+import { IsEnum, IsNumber } from 'class-validator';
+import { InventoryCountStatus } from 'src/inventory-count/domain/inventory-count.entity';
 import { CreateInventoryCountDto } from './create-inventory-count.dto';
 
-export class UpdateInventoryCountDto extends CreateInventoryCountDto {}
+export class UpdateInventoryCountDto extends PartialType(
+  CreateInventoryCountDto,
+) {
+  @IsEnum(InventoryCountStatus)
+  status: InventoryCountStatus;
+}
 
 export class UpdateInventoryCountItemDto {
   @IsNumber()
