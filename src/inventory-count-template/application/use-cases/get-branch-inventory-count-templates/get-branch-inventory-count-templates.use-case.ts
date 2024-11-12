e536d76ccd4893b7cf213731573c8aa9eda57925
Ansar <annssarr@gmail.com>
2024-11-13 00:22:06 +0500

    feat: inventory count template use cases

--- src/inventory-count-template/application/use-cases/get-branch-inventory-count-templates/get-branch-inventory-count-templates.use-case.ts
+++ src/inventory-count-template/application/use-cases/get-branch-inventory-count-templates/get-branch-inventory-count-templates.use-case.ts
@@ -1,22 +1,16 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { UseCase } from 'src/core/domain/use-case.interface';
 import { InventoryCountTemplateRepositoryPort } from 'src/inventory-count-template/domain/inventory-count-template-repository.port';
-import {
-  InventoryCountTemplateEntity,
-  InventoryCountTemplateType,
-} from 'src/inventory-count-template/domain/inventory-count-template.entity';
 import { InventoryCountTemplateDto as InventoryCountTemplateUi } from 'src/inventory-count-template/presentation/dto/inventory-count-template.dto';
 import { InventoryCountTemplateMapper } from '../../mappers/inventory-count-template.mapper';
 
 interface Props {
-  templateType: InventoryCountTemplateType;
   branchId: string;
-  name: string;
 }
 
 @Injectable()
-export class CreateInventoryCountTemplateUseCase
-  implements UseCase<Props, InventoryCountTemplateUi>
+export class GetBranchInventoryCountTemplatesUseCase
+  implements UseCase<Props, InventoryCountTemplateUi[]>
 {
   constructor(
     private readonly mapper: InventoryCountTemplateMapper,
@@ -25,14 +19,11 @@ export class CreateInventoryCountTemplateUseCase
     private readonly inventoryCountTemplateRepository: InventoryCountTemplateRepositoryPort,
   ) {}
 
-  async execute(props: Props): Promise<InventoryCountTemplateUi> {
-    const inventoryCountTemplate = InventoryCountTemplateEntity.create({
-      ...props,
-      storages: [],
-    });
-
-    await this.inventoryCountTemplateRepository.create(inventoryCountTemplate);
-
-    return this.mapper.toUi(inventoryCountTemplate);
+  async execute(props: Props): Promise<InventoryCountTemplateUi[]> {
+    const templates =
+      await this.inventoryCountTemplateRepository.findBranchInventoryCountTemplates(
+        props.branchId,
+      );
+    return templates.map((template) => this.mapper.toUi(template));
   }
 }
