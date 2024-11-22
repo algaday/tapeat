4447f58e84f81720ec0a1b61981c11918f653442
Kainar Massujima <masujimk@gmail.com>
2024-11-22 13:31:59 +0500

    feat: add notification subscription notification

--- src/telegram-chat/application/services/telegram-chat.application-service.ts
+++ src/telegram-chat/application/services/telegram-chat.application-service.ts
@@ -1,10 +1,13 @@
 import { Inject, Injectable } from '@nestjs/common';
+import { InjectBot } from 'nestjs-telegraf';
 import { NotificationSubscriptionCodeApplicationService } from 'src/notification-subscription-code/application/services/notification-subscription-code.application-service';
+import { RestaurantBranchService } from 'src/restaurant-branch/restaurant-branch.service';
 import { TelegramChatRepositoryPort } from 'src/telegram-chat/domain/telegram-chat-repository.port';
 import {
   TelegramChatEntity,
   TelegramChatType,
 } from 'src/telegram-chat/domain/telegram-chat.entity';
+import { Telegraf } from 'telegraf';
 
 type CreateChatParams = {
   secretCode: string;
@@ -17,9 +20,19 @@ export class TelegramChatApplicationService {
     @Inject(TelegramChatRepositoryPort)
     private readonly telegramChatRepository: TelegramChatRepositoryPort,
     private readonly notificationSubscriptionCodeApplicationService: NotificationSubscriptionCodeApplicationService,
+    private readonly restaurantBranchService: RestaurantBranchService,
+    @InjectBot() private readonly bot: Telegraf, // TODO: move to telegram module
   ) {}
 
   async createBySubscriptionSecretCode(params: CreateChatParams) {
+    const branch = await this.restaurantBranchService.getById(
+      params.restaurantBranchId,
+    );
+
+    if (!branch) {
+      throw new Error('Branch not found');
+    }
+
     return this.telegramChatRepository.transaction(async () => {
       const subscriptionCode =
         await this.notificationSubscriptionCodeApplicationService.activateBySecretCode(
@@ -34,7 +47,22 @@ export class TelegramChatApplicationService {
 
       await this.telegramChatRepository.create(telegramChat);
 
+      await this.notifySubscribed(
+        telegramChat.getProps().chatIdAtTelegram,
+        branch.address,
+      ).catch(console.error);
+
       return telegramChat;
     });
+  }
+
+  private async notifySubscribed(chatIdAtTelegram: string, branchName: string) {
+    await this.bot.telegram.sendMessage(
+      chatIdAtTelegram,
+      `Вы подписались на уведомление филиала ${branchName}`,
+      {
+        parse_mode: 'Markdown',
+      },
+    );
   }
 }
--- src/telegram-chat/infra/framework/telegram-chat.module.ts
+++ src/telegram-chat/infra/framework/telegram-chat.module.ts
@@ -5,6 +5,9 @@ import { TelegramChatMapper } from 'src/telegram-chat/application/mappers/telegr
 import { NotificationSubscriptionCodeController } from 'src/telegram-chat/presentation/notification-subscription-code.controller';
 import { TelegramChatApplicationService } from 'src/telegram-chat/application/services/telegram-chat.application-service';
 import { NotificationSubscriptionCodeModule } from 'src/notification-subscription-code/infra/framework/notification-subscription-code.module';
+import { TelegrafModule } from 'nestjs-telegraf';
+import { TelegramConfig } from 'src/config/telegram.config';
+import { RestaurantBranchModule } from 'src/restaurant-branch/restaurant-branch.module';
 
 const REPOSITORIES = [
   {
@@ -14,7 +17,13 @@ const REPOSITORIES = [
 ];
 
 @Module({
-  imports: [NotificationSubscriptionCodeModule],
+  imports: [
+    NotificationSubscriptionCodeModule,
+    RestaurantBranchModule,
+    TelegrafModule.forRoot({
+      token: TelegramConfig.TELEGRAM_BOT_TOKEN,
+    }),
+  ],
   controllers: [NotificationSubscriptionCodeController],
   providers: [
     TelegramChatMapper,
